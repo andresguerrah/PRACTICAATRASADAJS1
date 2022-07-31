99a72ab630e9538a3814d2ec83ffccdfab3a4166
andresguerrah <andres.guerrah@gmail.com>
2022-07-31 22:03:36 +0000

    casi

--- .learn/resets/14-Random_numbers/app.js
+++ .learn/resets/14-Random_numbers/app.js
@@ -2,10 +2,7 @@
 
 function getRandomInt()
 {
-	var randomNumber = Math.floor((Math.random()  )* 10);
-	if (randomNumber == 0){
-		randomNumber = 1
-	}
+	var randomNumber = Math.random();
 	return randomNumber;
 }
 
--- exercises/15-Rand_from_one_to_six/app.js
+++ exercises/15-Rand_from_one_to_six/app.js
@@ -1,6 +1,12 @@
 function getRandomInt()
 {
-	var randomNumber = Math.random();
-	return randomNumber;
+	var randomNumber = Math.floor(Math.random() * 10);
+	if (randomNumber >6) {
+		return randomNumber -4}
+	else if (randomNumber== 0){
+		return randomNumber + 1
+	}
+	else {return randomNumber}
+	
 }
 console.log(getRandomInt());
